bb2f86b5f9454bab7a83d253a598a13053e13149
Leandro Barros <leobarros.evangelista@gmail.com>
2019-06-03 14:52:51 -0300

    13.7. Adicionando campo com máscara (componente InputMask).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { DropdownModule } from 'primeng/dropdown';
+import {InputMaskModule} from 'primeng/inputmask';
 
 import { CurrencyMaskModule } from "ng2-currency-mask";
 
@@ -40,6 +41,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
+    InputMaskModule,
     CurrencyMaskModule 
     ],
   providers: [],
